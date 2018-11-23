f959264060ea871bb8b7af948cdb0cc5c906763c
Brent Bauman <brentbauman86@gmail.com>
2018-11-23 17:13:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solutions in this file
 
-function printBadges(names) {
-  for (let i = 0; i < names.length; i++)
+function printBadges(employees) {
+  for (let i = 0; i < employees.length; i++)
   {
-  console.log('Welcome ${names[i]}! You are employee #${i + 1}.');
+  console.log('Welcome ${employees[i]}! You are employee #${i + 1}.');
 }
 
-return names;
+return employees;
 }
