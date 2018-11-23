b08d4f08a975eef42f5d031e9a6744d78f013c0a
Brent Bauman <brentbauman86@gmail.com>
2018-11-23 17:16:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // Code your solutions in this file
 
-function printBadges(employees) {
-  for (let i = 0; i < employees.length; i++)
-  {
-  console.log('Welcome ${employees[i]}! You are employee #${i + 1}.');
+function printBadges (names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
+  }
+
+  return names;
 }
 
-return employees;
+function tailsNeverFails () {
+  let tails = 0;
+
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+
+  return `You got ${tails} tails in a row!`;
 }
